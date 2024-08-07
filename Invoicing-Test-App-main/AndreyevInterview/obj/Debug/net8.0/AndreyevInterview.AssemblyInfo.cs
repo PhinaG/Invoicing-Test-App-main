a460efa98d467f99bf62473510cf146ae276f94a
PhinaDavis <85176034+PhinaDavis@users.noreply.github.com>
2024-08-08 00:20:31 +0200

    cleanup

--- Invoicing-Test-App-main/AndreyevInterview/obj/Debug/net8.0/AndreyevInterview.AssemblyInfo.cs
+++ Invoicing-Test-App-main/AndreyevInterview/obj/Debug/net8.0/AndreyevInterview.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AndreyevInterview")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+769361c16980c5e5d3848e7b85b551126257e962")]
 [assembly: System.Reflection.AssemblyProductAttribute("AndreyevInterview")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AndreyevInterview")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
